1a745c765035319dc6850a187ab08c65c8bb41d1
kinothUI <crypt0@runbox.at>
2020-07-19 13:52:33 +0200

    api: remove anonymous async functions from controllers

--- controllers/logoController.js
+++ controllers/logoController.js
@@ -1,55 +1,48 @@
 const mysql = require("../mysql/mysql-connector");
 const fs = require("fs");
 
-
-const getAllLogos = (req, res) => {
-  (async () => {
-    try {
-      const SELECT_LOGOS_QUERY = `SELECT * FROM logo;`;
-      let response = await mysql.query(SELECT_LOGOS_QUERY);
-      res.json({
-        data: response.data
-      });
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const getAllLogos = async (req, res) => {
+  try {
+    const SELECT_LOGOS_QUERY = `SELECT * FROM logo;`;
+    let response = await mysql.query(SELECT_LOGOS_QUERY);
+    res.json({
+      data: response.data,
+    });
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const uploadLogo = (req, res) => {
-  (async () => {
-    try {
-      const uuid = req.body.uuid;
-      const filename = req.file.filename;
-      const INSERT_LOGO_QUERY = `INSERT INTO logo (uuid, src, thumbnail, thumbnailWidth, thumbnailHeight) VALUES('${uuid}','${filename}', '${filename}', '150', '85');`;
-      await mysql.query(INSERT_LOGO_QUERY);
-      res.status(200).send({ uuid, filename });
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const uploadLogo = async (req, res) => {
+  try {
+    const uuid = req.body.uuid;
+    const filename = req.file.filename;
+    const INSERT_LOGO_QUERY = `INSERT INTO logo (uuid, src, thumbnail, thumbnailWidth, thumbnailHeight) VALUES('${uuid}','${filename}', '${filename}', '150', '85');`;
+    await mysql.query(INSERT_LOGO_QUERY);
+    res.status(200).send({ uuid, filename });
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const deleteLogo = (req, res) => {
-  (async () => {
-    try {
-      const { filename } = req.params;
-      const DELETE_LOGO_QUERY = `DELETE FROM logo WHERE src='${filename}';`;
-      let response = await mysql.query(DELETE_LOGO_QUERY);
-      res.json({
-        data: response.data
-      });
-      fs.unlink(`./uploads/${filename}`, err => {
-        err ? console.error(err) : null;
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  })();
+const deleteLogo = async (req, res) => {
+  try {
+    const { filename } = req.params;
+    const DELETE_LOGO_QUERY = `DELETE FROM logo WHERE src='${filename}';`;
+    let response = await mysql.query(DELETE_LOGO_QUERY);
+    res.json({
+      data: response.data,
+    });
+    fs.unlink(`./uploads/${filename}`, (err) => {
+      err ? console.error(err) : null;
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 module.exports = {
   getAllLogos,
   uploadLogo,
-  deleteLogo
+  deleteLogo,
 };
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,70 +1,53 @@
 const mysql = require("../mysql/mysql-connector");
 
-const getAllOrders = (req, res) => {
-  (async () => {
-    try {
-      const SELECT_ALL_ORDERS_QUERY = "SELECT * FROM orders;";
-      let response = await mysql.query(SELECT_ALL_ORDERS_QUERY);
-      res.json({
-        data: response.data
-      });
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const getAllOrders = async (req, res) => {
+  try {
+    const SELECT_ALL_ORDERS_QUERY = "SELECT * FROM orders;";
+    let response = await mysql.query(SELECT_ALL_ORDERS_QUERY);
+    res.json({
+      data: response.data,
+    });
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const addOrder = (req, res) => {
-  (async () => {
-    try {
-      const {
-        uuid,
-        xchange,
-        ticker,
-        type,
-        lmt,
-        volume,
-        comment,
-        date
-      } = req.query;
-      const INSERT_ORDERS_QUERY = `INSERT INTO orders (uuid, xchange, ticker, type, lmt, volume, comment, date) VALUES('${uuid}','${xchange}','${ticker}','${type}',${lmt},${volume},'${comment}','${date}');`;
-      await mysql.query(INSERT_ORDERS_QUERY);
-      res.send("order added successfully");
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const addOrder = async (req, res) => {
+  try {
+    const { uuid, xchange, ticker, type, lmt, volume, comment, date } = req.query;
+    const INSERT_ORDERS_QUERY = `INSERT INTO orders (uuid, xchange, ticker, type, lmt, volume, comment, date) VALUES('${uuid}','${xchange}','${ticker}','${type}',${lmt},${volume},'${comment}','${date}');`;
+    await mysql.query(INSERT_ORDERS_QUERY);
+    res.send("order added successfully");
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const fillOrder = (req, res) => {
-  (async () => {
-    try {
-      const { uuid, filled } = req.query;
-      const UPDATE_ORDERS_FILLED = `UPDATE orders SET filled=${filled} WHERE uuid='${uuid}';`;
-      await mysql.query(UPDATE_ORDERS_FILLED);
-      res.send("order status updated successfully");
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const fillOrder = async (req, res) => {
+  try {
+    const { uuid, filled } = req.query;
+    const UPDATE_ORDERS_FILLED = `UPDATE orders SET filled=${filled} WHERE uuid='${uuid}';`;
+    await mysql.query(UPDATE_ORDERS_FILLED);
+    res.send("order status updated successfully");
+  } catch (error) {
+    res.send(error);
+  }
 };
 
-const deleteOrder = (req, res) => {
-  (async () => {
-    try {
-      const { uuid } = req.params;
-      const DELETE_ORDER_QUERY = `DELETE FROM orders WHERE uuid='${uuid}';`;
-      let response = await mysql.query(DELETE_ORDER_QUERY);
-      res.send("order deleted successfully" + response.results);
-    } catch (error) {
-      res.send(error);
-    }
-  })();
+const deleteOrder = async (req, res) => {
+  try {
+    const { uuid } = req.params;
+    const DELETE_ORDER_QUERY = `DELETE FROM orders WHERE uuid='${uuid}';`;
+    let response = await mysql.query(DELETE_ORDER_QUERY);
+    res.send("order deleted successfully" + response.results);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 module.exports = {
   getAllOrders,
   addOrder,
   fillOrder,
-  deleteOrder
+  deleteOrder,
 };
