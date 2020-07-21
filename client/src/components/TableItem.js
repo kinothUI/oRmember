ceacc83631482dd60167c563c5d4148152fb1cb4
kinothUI <crypt0@runbox.at>
2020-07-22 00:18:36 +0200

    ui: restructured code

--- client/src/components/TableItem.js
+++ client/src/components/TableItem.js
@@ -10,7 +10,7 @@ import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "semantic-ui-css/semantic.min.css";
 
 class TableItem extends Component {
-  convertedTimestamp = timestamp => {
+  convertedTimestamp = (timestamp) => {
     let theDate = new Date(timestamp);
     let day = theDate.getDate();
     let month = theDate.getMonth();
@@ -24,147 +24,51 @@ class TableItem extends Component {
     this.props.uploadLogo(uuid, logo);
   };
 
-  deleteOrder = uuid => {
+  deleteOrder = (uuid) => {
     this.props.deleteOrder(uuid);
   };
 
   render() {
-    const selected = this.props.orders.map(orders => {
-      if (orders.filled) {
-        return orders.uuid;
-      } else {
-        return false;
-      }
-    });
-
-    const selectRow = {
-      mode: "checkbox",
-      clickToSelect: false,
-      clickToExpand: true,
-      hideSelectAll: true,
-      onSelect: row => {
-        this.props.toggleFilled(row.uuid);
-      },
-      selected,
-      style: { textDecoration: "line-through", backgroundColor: "#c8e6c9" }
-    };
-
-    const expandRow = {
-      showExpandColumn: false,
-      renderer: row => {
-        const { deleteLogo } = this.props;
-        return (
-          <div>
-            <table className='expandRowTable'>
-              <tbody>
-                <tr>
-                  <td rowSpan='2'>
-                    <div>
-                      <Segment>
-                        <Header as='h4' textAlign='center'>
-                          Comment{" "}
-                          <Icon
-                            name='comment alternate outline'
-                            color={_.isEmpty(row.comment) ? "red" : "black"}
-                          />
-                        </Header>
-                        <Divider />
-                        {row.comment}
-                        {_.isEmpty(row.comment) ? null : <Divider />}
-                        <p className='date'>
-                          Setup set @{" "}
-                          {new Intl.DateTimeFormat("de-DE", {
-                            year: "numeric",
-                            month: "long",
-                            weekday: "long",
-                            day: "numeric",
-                            hour: "numeric",
-                            hour12: false,
-                            minute: "numeric"
-                          }).format(row.date)}
-                        </p>
-                      </Segment>
-                    </div>
-                  </td>
-                  <td>
-                    {row.logo && row.logo.length > 0 ? (
-                      <ImageGallery images={row.logo} deleteLogo={deleteLogo} />
-                    ) : (
-                      <Dropzone
-                        onDrop={this.onImageDrop.bind(this, row.uuid)}
-                        accept='image/jpg,image/jpeg,image/png'
-                        multiple={true}
-                      >
-                        {({ getRootProps, getInputProps }) => {
-                          return (
-                            <div {...getRootProps()}>
-                              <input {...getInputProps()} />
-                              {
-                                <p className='fileDrop'>
-                                  Try dropping one or more files here
-                                </p>
-                              }
-                            </div>
-                          );
-                        }}
-                      </Dropzone>
-                    )}
-                  </td>
-                </tr>
-                <tr>
-                  <td align='right' style={{ verticalAlign: "bottom" }}>
-                    <Button
-                      onClick={this.deleteOrder.bind(this, row.uuid)}
-                      size='small'
-                      icon='trash alternate outline'
-                      color='red'
-                      circular
-                    />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        );
-      }
-    };
+    const selected = this.getFilledOrders();
+    const selectRow = this.selectRowProps(selected);
+    const expandRow = this.renderExpandRow();
 
     const columns = [
       {
         dataField: "id",
         text: "ID",
-        hidden: true
+        hidden: true,
       },
       {
         dataField: "xchange",
-        text: "Exchange"
+        text: "Exchange",
       },
       {
         dataField: "ticker",
-        text: "Symbol"
+        text: "Symbol",
       },
       {
         dataField: "type",
-        text: "Type"
+        text: "Type",
       },
       {
         dataField: "lmt",
-        text: "Limit"
+        text: "Limit",
       },
       {
         dataField: "volume",
-        text: "Volume"
+        text: "Volume",
       },
       {
         dataField: "comment",
         text: "Comment",
-        hidden: true
+        hidden: true,
       },
       {
         dataField: "date",
         text: "Datum",
-        hidden: true
-      }
+        hidden: true,
+      },
     ];
 
     return (
@@ -183,6 +87,101 @@ class TableItem extends Component {
       </div>
     );
   }
+
+  getFilledOrders() {
+    const { orders } = this.props;
+
+    return orders.map((orders) => (orders.filled && orders.uuid) || false);
+  }
+
+  selectRowProps = (selected) => ({
+    mode: "checkbox",
+    clickToSelect: false,
+    clickToExpand: true,
+    hideSelectAll: true,
+    onSelect: (row) => {
+      this.props.toggleFilled(row.uuid);
+    },
+    selected,
+    style: { textDecoration: "line-through", backgroundColor: "#c8e6c9" },
+  });
+
+  renderExpandRow = () => ({
+    showExpandColumn: false,
+    renderer: (row) => {
+      const { deleteLogo } = this.props;
+
+      return (
+        <div>
+          <table className='expandRowTable'>
+            <tbody>
+              <tr>
+                <td rowSpan='2'>
+                  <div>
+                    <Segment>
+                      <Header as='h4' textAlign='center'>
+                        Comment{" "}
+                        <Icon
+                          name='comment alternate outline'
+                          color={_.isEmpty(row.comment) ? "red" : "black"}
+                        />
+                      </Header>
+                      <Divider />
+                      {row.comment}
+                      {_.isEmpty(row.comment) ? null : <Divider />}
+                      <p className='date'>
+                        Setup set @{" "}
+                        {new Intl.DateTimeFormat("de-DE", {
+                          year: "numeric",
+                          month: "long",
+                          weekday: "long",
+                          day: "numeric",
+                          hour: "numeric",
+                          hour12: false,
+                          minute: "numeric",
+                        }).format(row.date)}
+                      </p>
+                    </Segment>
+                  </div>
+                </td>
+                <td>
+                  {row.logo && row.logo.length > 0 ? (
+                    <ImageGallery images={row.logo} deleteLogo={deleteLogo} />
+                  ) : (
+                    <Dropzone
+                      onDrop={this.onImageDrop.bind(this, row.uuid)}
+                      accept='image/jpg,image/jpeg,image/png'
+                      multiple={true}
+                    >
+                      {({ getRootProps, getInputProps }) => {
+                        return (
+                          <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {<p className='fileDrop'>Try dropping one or more files here</p>}
+                          </div>
+                        );
+                      }}
+                    </Dropzone>
+                  )}
+                </td>
+              </tr>
+              <tr>
+                <td align='right' style={{ verticalAlign: "bottom" }}>
+                  <Button
+                    onClick={this.deleteOrder.bind(this, row.uuid)}
+                    size='small'
+                    icon='trash alternate outline'
+                    color='red'
+                    circular
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      );
+    },
+  });
 }
 
 export default TableItem;
